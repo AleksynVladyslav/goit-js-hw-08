02b70617541a2c10407f05d8e14d2e0bdd6d223f
Vladyslav Aleksyn <120416059+AleksynVladyslav@users.noreply.github.com>
2023-05-22 21:33:49 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,11 +4,11 @@ const STORAGE_DATA_KEY = 'feedback-form-state';
 const dataForm = {};
 const refs = {
   formRef: document.querySelector('.feedback-form'),
-  emailRef: document.querySelector('feedback-form input'),
-  messageRef: document.querySelector('feedback-form textarea'),
+  emailRef: document.querySelector('.feedback-form input'),
+  messageRef: document.querySelector('.feedback-form textarea'),
 };
 
-refs.formRef.addEventListener('input', onInputsData);
+refs.formRef.addEventListener('input', throttle(onInputsData, 500));
 refs.formRef.addEventListener('submit', onFormSubmit);
 
 function onInputsData(event) {
@@ -18,13 +18,17 @@ function onInputsData(event) {
 
 function populateForm() {
   const savedData = JSON.parse(localStorage.getItem(STORAGE_DATA_KEY));
-  console.log(savedData);
   if (savedData) {
-    refs.emailRef.value = savedData.email;
-    refs.messageRef.value = savedData.message;
+    refs.emailRef.value = savedData.email || '';
+    refs.messageRef.value = savedData.message || '';
   }
 }
-
 populateForm();
 
-function onFormSubmit(event) {}
+function onFormSubmit(event) {
+  event.preventDefault()
+console.log(`email: ${refs.emailRef.value}, message: ${refs.messageRef.value}`)
+  localStorage.removeItem(STORAGE_DATA_KEY);
+  event.currentTarget.reset()
+  
+}
